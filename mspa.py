2c9016aaae5962999abb1a2c38716289a844897c
ri <ri@scaegroup.com>
2020-12-11 19:23:18 +0300

    .

--- mspa.py
+++ mspa.py
@@ -2,11 +2,14 @@ import gmsh
 import math
 import os
 import sys
+import numpy as np
+
 
 MODEL_NAME = 'mspa'
 
 mm = 1.0e-3
 dh = 1.5 * mm
+dc = 0.35 * mm
 w0 = 55.49 * mm
 l0 = 42.99 * mm
 
@@ -31,9 +34,15 @@ mesh_size_substrate = 1.5 * mm
 mesh_size_environment = 10 * mm
 
 gmsh.initialize()
-gmsh.open(MODEL_NAME + '.pro')
 
-gmsh.model.add(MODEL_NAME)
+gmsh.open('mspa.pro')
+gmsh.onelab.run()
+
+# gmsh.model.setCurrent('mspa.geo')
+# path = os.path.dirname(os.path.abspath(__file__))
+# gmsh.merge(os.path.join(path, 'mspa.pro'))
+
+gmsh.model.add(MODEL_NAME + '.geo')
 occ = gmsh.model.occ
 
 patch2d = occ.addRectangle(-w0 * 0.5, -l0 * 0.5, dh, w0, l0)
@@ -41,12 +50,25 @@ feed2d = occ.addRectangle(-w_feed * 0.5, l0 * 0.5, dh, w_feed, (l1 - l0) * 0.5)
 tags, _ = occ.fuse([(2, patch2d)], [(2, feed2d)], 0)
 patch2d = tags[0][1]
 
+tags = occ.extrude([(2, patch2d)], 0.0, 0.0, dc)
+patch3d = tags[1][1]
+
 pcb2d = occ.addRectangle(-w1 * 0.5, -l1 * 0.5, 0.0, w1, l1)
+
+port_point0 = occ.addPoint(-w_feed * 0.5, l1 * 0.5, 0.0)
+port_point1 = occ.addPoint(w_feed * 0.5, l1 * 0.5, 0.0)
+tags, _ = occ.fragment([(2, pcb2d)], [(0, port_point0),
+                                      (0, port_point1)])
+pcb2d = tags[0][1]
 tags = occ.extrude([(2, pcb2d)], 0.0, 0.0, dh)
 pcb3d = tags[1][1]
 
+tags = occ.extrude([(2, pcb2d)], 0.0, 0.0, -dc)
+gnd3d = tags[1][1]
+
 air3d = occ.addBox(-w2 * 0.5, -l2 * 0.5, (dh - h2) * 0.5, w2, l2, h2)
-tags, _ = occ.cut([(3, air3d)], [(3, pcb3d)], 0, True, False)
+tags, _ = occ.cut([(3, air3d)], [(3, pcb3d), (3, gnd3d),
+                                 (3, patch3d)], 0, True, False)
 air3d = tags[0][1]
 
 pml3d = occ.addBox(-w3 * 0.5, -l3 * 0.5, (dh - h3) * 0.5, w3, l3, h3)
@@ -63,8 +85,14 @@ gmsh.option.setNumber('Mesh.Smoothing', 5)
 tag = gmsh.model.addPhysicalGroup(2, [patch2d, pcb2d])
 gmsh.model.setPhysicalName(2, tag, 'Conductor')
 
-tag = gmsh.model.addPhysicalGroup(2, [patch2d])
+eps = 1.0e-4
+tags = gmsh.model.occ.getEntitiesInBoundingBox(
+    -w_feed * 0.5 - eps, l1 * 0.5 - eps, -eps, w_feed * 0.5 + eps, l1 * 0.5 + eps, dh + eps, 2)
+tag = gmsh.model.addPhysicalGroup(2, [tags[0][1]])
 gmsh.model.setPhysicalName(2, tag, 'SkinFeed')
+
+tag = gmsh.model.addPhysicalGroup(3, [patch3d, gnd3d])
+gmsh.model.setPhysicalName(3, tag, 'ConductorVolume')
 
 tag = gmsh.model.addPhysicalGroup(3, [pcb3d])
 gmsh.model.setPhysicalName(3, tag, 'Substrate')
@@ -75,23 +103,18 @@ gmsh.model.setPhysicalName(3, tag, 'Air')
 tag = gmsh.model.addPhysicalGroup(3, [pml3d])
 gmsh.model.setPhysicalName(3, tag, 'Pml')
 
-tag = gmsh.model.addPhysicalGroup(2, [patch2d, pcb2d])
-gmsh.model.setPhysicalName(2, tag, 'DomainTot')
 
-
-# gmsh.model.mesh.setSize(gmsh.model.occ.getEntities(0), mesh_size_condutor)
 tags = gmsh.model.getBoundary([(3, pcb3d)], False, False, True)
 gmsh.model.mesh.setSize(tags, mesh_size_substrate)
-# gmsh.model.setColor([(3, pcb3d)], 255, 0, 0, 8, True)
 
 tags = gmsh.model.getBoundary([(3, air3d), (3, pml3d)], False, False, True)
 gmsh.model.mesh.setSize(tags, mesh_size_environment)
-# gmsh.model.setColor([(3, air3d), (3, pml3d)], 0, 0, 255, 8, True)
 
-tags = gmsh.model.getBoundary([(2, patch2d), (2, pcb2d)], False, False, True)
+tags = gmsh.model.getBoundary(
+    [(3, patch3d), (3, gnd3d)], False, False, True)
 gmsh.model.mesh.setSize(tags, mesh_size_condutor)
 
-gmsh.model.mesh.generate(2)
+gmsh.model.mesh.generate(3)
 gmsh.write(MODEL_NAME + '.msh')
 
 # if '-nopopup' not in sys.argv:
--- mstrip.py
+++ mstrip.py
@@ -6,6 +6,37 @@ from math import pi
 import gmsh
 import mspa
 
+
+GDICT1 = {
+    'Point': 1,
+    'Line': 3,
+    'Triangle': 4,
+    'Quadrangle': 4,
+    'Tetrahedron': 4,
+    'Hexahedron': 6,
+    'Prism': 6,
+}
+
+GDICT2 = {
+    'Point': 1,
+    'Line': 4,
+    'Triangle': 7,
+    'Quadrangle': 7,
+    'Tetrahedron': 15,
+    'Hexahedron': 34,
+    'Prism': 21,
+}
+
+
+def add_integration(integration, name, group_dict, itype='Gauss'):
+    i0 = integration.add(name)
+    item = i0.add()
+    item_case = item.add(itype)
+    ici = item_case.add()
+    for element, value in group_dict.items():
+        ici.add(GeoElement=element, NumberOfPoints=value)
+
+
 pro = Problem()
 pro.filename = mspa.MODEL_NAME + '.pro'
 groups = gmsh.model.getPhysicalGroups()
@@ -13,8 +44,16 @@ for g in groups:
     tag = g[1]
     name = gmsh.model.getPhysicalName(g[0], g[1])
     pro.group.add(name, tag)
+pro.group.Region('SurBC', 'SkinFeed')
+pro.group.Region('DomainTot', ['SkinFeed', 'Air', 'Pml'])
+pro.group.Region('DomainC', [])  # TODO remove
+pro.group.Region('Domain', ['Substrate', 'Air', 'Pml'])
+pro.group.define('DomainS')  # TODO remove
+pro.group.define('SurS')  # TODO remove
+pro.group.ElementsOf('TrGr', 'Domain', OnOneSideOf='SkinFeed')
 
-# print_html(group.code)
+freq = 1.6e9  # Hz~1.0/s
+cvel = 299792458.0  # m/s
 
 fvar = {}
 fvar['mu0'] = 4.0e-7 * pi
@@ -23,7 +62,17 @@ fvar['ep0'] = 8.854187817e-12
 fvar['epr'] = 4.7  # Dielectric constant for FR4 is ~4.5
 fvar['zl'] = 50.0  # Ohms load resistance
 fvar['eta0'] = 120.0 * pi  # eta0 = Sqrt(mu0/eps0)
-fvar['freq'] = 1.6e9  # MHz
+fvar['freq'] = freq
+fvar['k0'] = 2.0 * pi * freq / cvel
+
+fvar['pml_xmax'] = 0.055
+fvar['pml_ymax'] = 0.042
+fvar['pml_zmax'] = 0.010
+fvar['pml_xmin'] = -0.003
+fvar['pml_ymin'] = -0.003
+fvar['pml_zmin'] = -0.003
+fvar['pml_delta'] = 12.0 * (mspa.dc + mspa.dh)
+
 
 f = pro.function
 
@@ -31,8 +80,26 @@ for name, value in fvar.items():
     f.constant(name, value)
 
 f.add('I', f.Complex(0, 1))
-f.add('epsilon', 'ep0', region=['Air', 'SkinFeed', 'Pml'])
+f.add('epsilon', 'ep0', region=['Air', 'SkinFeed'])
 f.add('epsilon', 'epr * ep0', region='Substrate')
+f.add('nu', 'nu0', region=['Air', 'Substrate', 'SkinFeed'])
+
+f.add('sigma', '6.0e7')  # Copper
+f.define('js0')  # TODO remove
+f.define('ks0')  # TODO remove
+f.define('nxh')  # TODO remove
+
+f.add('DampingProfileX', '(X[] >= pml_xmax || X[] <= pml_xmin) ? (X[] >= pml_xmax ? 1.0 / (pml_delta - (X[] - pml_xmax)): 1.0 / (pml_delta - (pml_xmin - X[]))): 0.0')
+f.add('DampingProfileY', '(Y[] >= pml_ymax || Y[] <= pml_ymin) ? (Y[] >= pml_ymax ? 1.0 / (pml_delta - (Y[] - pml_ymax)): 1.0 / (pml_delta - (pml_ymin - Y[]))): 0.0')
+f.add('DampingProfileZ', '(Z[] >= pml_zmax || Z[] <= pml_zmin) ? (Z[] >= pml_zmax ? 1.0 / (pml_delta - (Z[] - pml_zmax)): 1.0 / (pml_delta - (pml_zmin - Z[]))): 0.0')
+
+f.add('cx', f.Complex(1.0, '-DampingProfileX[] / k0'))
+f.add('cy', f.Complex(1.0, '-DampingProfileY[] / k0'))
+f.add('cz', f.Complex(1.0, '-DampingProfileZ[] / k0'))
+f.add('tens', f.TensorDiag('cy[] * cz[] / cx[]',
+                           'cx[] * cz[] / cy[]', 'cx[] * cy[] / cz[]'))
+f.add('epsilon', 'ep0 * tens[]', region='Pml')
+f.add('nu', 'nu0 / tens[]', region='Pml')
 
 # D5 = 1.40 * mm ;
 # V0 = 1 ; delta_gap = D5 ;
@@ -56,60 +123,98 @@ for js, s in enumerate(['Vol', 'Sur']):
     jacobian.items[js].cases[0].add(Region="All", Jacobian=s)
 
 fspace = pro.functionspace
-fs0 = fspace.add('Hcurl_e', Type='Form1')
-fs0.add_basis_function(
+fs = fspace.add('Hcurl_e', Type='Form1')
+fs.add_basis_function(
     Name='se',
     NameOfCoef='ee',
     Function='BF_Edge',
     Support='DomainTot',
     Entity='EdgesOf[All]'
 )
-fs0.add_constraint(NameOfCoef='ee', EntityType='EdgesOf',
-                   NameOfConstraint='ElectricField')
+fs.add_constraint(NameOfCoef='ee', EntityType='EdgesOf',
+                  NameOfConstraint='ElectricField')
+fs = fspace.add('Hcurl_h', Type='Form1')
+fs.add_basis_function(
+    Name='sh',
+    NameOfCoef='he',
+    Function='BF_Edge',
+    Support='DomainTot',
+    Entity='EdgesOf[All]'
+)
 
-integration = pro.integration
-i0 = integration.add("I2")
-item = i0.add()
-item_case = item.add("Gauss")
-ici = item_case.add()
-
-gdict = {
-    'Point': 1,
-    'Line': 4,
-    'Triangle': 7,
-    'Quadrangle': 7,
-    'Tetrahedron': 15,
-    'Hexahedron': 34,
-    'Prism': 21,
-}
-
-for name, value in gdict.items():
-    ici.add(GeoElement=name, NumberOfPoints=value)
+add_integration(pro.integration, 'I1', GDICT1)
+add_integration(pro.integration, 'I2', GDICT2)
 
 formulation = pro.formulation
-f0 = formulation.add('Microwave_e_BC', Type='FemEquation')
-q = f0.add_quantity()
+f = formulation.add('Microwave_e_BC', Type='FemEquation')
+q = f.add_quantity()
 q.add(Name='e', Type='Local', NameOfSpace='Hcurl_e')
-e = f0.add_equation()
-e.add('Galerkin', 'Dof{e} , {e}', In='SkinFeed',
+e = f.add_equation()
+e.add('Galerkin', '', 'Dof{e} , {e}', In='SurBC',
       Integration='I2', Jacobian='JSur')
-e.add('Galerkin', '-BC_Fct_e[] , {e}',
-      In='SkinFeed', Integration='I2', Jacobian='JSur')
+e.add('Galerkin', '', '-BC_Fct_e[] , {e}',
+      In='SurBC', Integration='I2', Jacobian='JSur')
+
+f = formulation.add('Microwave_e', Type='FemEquation')
+q = f.add_quantity()
+q.add(Name='e', Type='Local', NameOfSpace='Hcurl_e')
+q.add(Name='h', Type='Local', NameOfSpace='Hcurl_h')
+
+e = f.add_equation()
+e.add('Galerkin', 'DtDof', 'nu[] * Dof{d e} , {d e}', In='Domain',
+      Integration='I1', Jacobian='JVol')
+e.add('Galerkin', '', 'sigma[] * Dof{e}, {e}',
+      In='DomainC', Integration='I1', Jacobian='JVol')
+e.add('Galerkin', 'DtDtDof', 'epsilon[] * Dof{e} , {e}',
+      In='Domain', Integration='I1', Jacobian='JVol')
+e.add('Galerkin', 'DtDof', 'js0[], {e}',
+      In='DomainS', Integration='I1', Jacobian='JVol')
+e.add('Galerkin', 'DtDof', '-ks0[] , {d e}',
+      In='DomainS', Integration='I1', Jacobian='JVol')
+e.add('Galerkin', 'DtDof', '-nxh[] , {e}',
+      In='SurS', Integration='I1', Jacobian='JSur')
+
+# // store magnetic field for Admitance computation (Yin)
+e.add('Galerkin', '', 'Dof{h} , {h}', In='TrGr',
+      Integration='I1', Jacobian='JVol')
+e.add('Galerkin', '', '-I[]*nu[]*Dof{d e}/(2*Pi*freq), {h}',
+      In='TrGr', Integration='I1', Jacobian='JVol')
 
 resolution = pro.resolution
-res0 = resolution.add('Microwave_e_BC')
-s = res0.add_system()
-# s.add(Name='B', NameOfFormulation='Microwave_e_BC', DestinationSystem='A')
-s.add(Name='B', NameOfFormulation='Microwave_e_BC')
-operation = res0.add_operation()
+
+res = resolution.add('Microwave_e_BC; Hidden 1')
+s = res.add_system()
+s.add(Name='B', NameOfFormulation='Microwave_e_BC', DestinationSystem='A')
+
+operation = res.add_operation()
 operation.Generate('B')
 operation.Solve('B')
-operation.SaveSolution('B')
-# operation.TransferSolution('B')
+operation.TransferSolution('B')
+
+res = resolution.add('Analysis')
+s = res.add_system()
+s.add(Name='A', NameOfFormulation='Microwave_e',
+      Type='Complex', Frequency='freq')
+
+operation = res.add_operation()
+operation.CreateDirectory('build')
+operation.Generate('A')
+operation.Solve('A')
+operation.SaveSolution('A')
+
+# pp = pro.postprocessing
+# pp.add('pp_test', 'Microwave_e')
+# po = pro.postoperation
+# poi = po.add('po_test', 'pp_test')
+# poi.add().add("v", OnElementsOf="Domain", File="test.pos")
+
 
 pro.make_file()
 pro.write_file()
-# gmsh.merge(pro.filename)
+# gmsh.open(pro.filename)
+# gmsh.onelab.run()
+# gmsh.model.setCurrent('mspa.geo')
+
 
 if '-nopopup' not in sys.argv:
     gmsh.fltk.run()
